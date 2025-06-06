cede3af1be31e2a4e0cf56b08ea548246bdad708
rbassoli1980 <rbassoli@kyndryl.com>
2025-06-06 11:24:59 -0300

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -123,4 +123,12 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    # ADD YOUR TEST CASES HERE ...
+    def test_get_account(self):
+            """It should Read a single Account"""
+            account = self._create_accounts(1)[0]
+            resp = self.client.get(
+                f"https://github.com/rbassoli1980", content_type="application/json"
+            )
+            self.assertEqual(resp.status_code, status.HTTP_200_OK)
+            data = resp.get_json()
+            self.assertEqual(data["name"], account.name)
